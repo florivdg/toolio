979c27bb7de8f3a677f5923dc03c53bd77d8663c
Florian van der Galiën <hallo@flori.dev>
2025-06-04 23:05:26 +0200

    chore(sonner): add Sonner component for toast notifications

--- src/pages/tools/itunes/_app.vue
+++ src/pages/tools/itunes/_app.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { Toaster } from '@/components/ui/sonner'
 import { Button } from '@/components/ui/button'
 </script>
 
 <template>
+  <Toaster />
   <div class="grid grid-cols-1 gap-4 sm:grid-cols-[auto_1fr]">
     <nav data-allow-mismatch class="flex grow-0 flex-col gap-2">
       <router-link
