c29e54bcf598a7ff4758c93e9899da1357b3d26d
Florian van der Galiën <hallo@flori.dev>
2025-07-03 22:01:20 +0200

    fix(app): Pinia Colada setup #11

--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,15 +1,15 @@
 import type { App } from 'vue'
-import { createPinia, type Pinia } from 'pinia'
+import { createPinia } from 'pinia'
 import { PiniaColada } from '@pinia/colada'
 import { clientRouter } from './_clientRouter'
 
 export default (app: App) => {
   // Set up Pinia
-  const pinia: Pinia = createPinia()
-  
+  const pinia = createPinia()
+
   // Set up Pinia Colada
-  pinia.use(PiniaColada)
   app.use(pinia)
+  app.use(PiniaColada)
 
   if (clientRouter) app.use(clientRouter)
 }
